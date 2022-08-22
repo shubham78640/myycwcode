3472f0ba1a6025c29f7c2d1765c5f6acae3e054d
shubham78640 <shubham@thepinchlife.com>
2022-08-22 11:23:43 +0530

    updated my ycw code

---
+++ vista-main/vista-main/src/components/Details/JobDescription/JobsAllDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import YcwNav from '../YCWNav'
+import JobDetails from './JobDetails'
+
+function JobsAllDetails() {
+  return (
+    <>
+    <YcwNav/>
+    <JobDetails/>
+    
+
+    </>
+  )
+}
+
+export default JobsAllDetails
